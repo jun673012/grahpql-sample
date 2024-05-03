0723ca04e4e320ae136cffb92a142d188bab1b18
junhyeok <jun673012@gmail.com>
2024-05-03 17:59:53 +0900

    init

---
+++ src/main/java/com/example/qlql/QlqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.qlql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qlql/QlqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qlql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
